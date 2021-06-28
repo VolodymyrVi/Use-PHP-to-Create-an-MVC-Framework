de04717c8ba86627ca9cd6d7984ed1d0b69c5699
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-28 10:57:14 +0300

    added get method

--- core/Router.php
+++ core/Router.php
@@ -8,5 +8,11 @@ namespace app\core;
  */
 class Router
 {
+    protected array $routes = [];
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
 
 }
