3390dcb1358535f816ba07e3e0da544ff7407e22
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-28 14:57:56 +0300

    added method resolve - var_dumb -

--- core/Router.php
+++ core/Router.php
@@ -14,5 +14,11 @@ class Router
         $this->routes['get'][$path] = $callback;
     }
 
-
+    public function resolve()
+    {
+        echo '<pre>';
+        var_dump($_SERVER);
+        echo '<pre>';
+        exit;
+    }
 }
